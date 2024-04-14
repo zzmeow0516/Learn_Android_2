837022f647be7ba10103a045de0fadc58072b827
zzmeow0516 <zzmeow0516@163.com>
2024-04-15 00:36:42 +0800

    bring learn Android 2

---
+++ app/src/main/java/com/example/learn_android_2/DialogActivity.kt
@@ -0,0 +1,15 @@
+package com.example.learn_android_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DialogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dialog)
+    }
+}
---
+++ app/src/main/java/com/example/learn_android_2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.learn_android_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "mylog_MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
